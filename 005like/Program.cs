33e46fa52b3d18fd4f0a5fd37ec62abcbb8d904b
Matskevich_Hanna <hankohanna@mail.ru>
2022-09-02 23:34:56 +0300

    любимчик Миша

--- 005like/Program.cs
+++ 005like/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "миша")
+{
+    Console.WriteLine("Ура, это же Миша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
